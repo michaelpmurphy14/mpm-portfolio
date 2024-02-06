c321c5056b1fb7f2573fe2a3961d79609d6a0eda
michaelpmurphy14 <135272573+michaelpmurphy14@users.noreply.github.com>
2024-02-06 17:14:20 -0600

    Create RoleInterests.js

---
+++ src/RoleInterests.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoleInterests = () => {
+    return (
+        <section id="role-interests" className="p-5 bg-light">
+            <h2>Role & Interests</h2>
+            <p>MEP Consulting Design Engineer -> Innovation Designer -> Product Designer & Owner</p>
+            <p>Interests: Technology, Security, Cloud, UX, A&D</p>
+        </section>
+    );
+};
+
+export default RoleInterests;
