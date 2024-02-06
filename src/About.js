88a5e54679d813022d3ad16f0341dfbd06165f21
michaelpmurphy14 <135272573+michaelpmurphy14@users.noreply.github.com>
2024-02-06 17:13:53 -0600

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section id="about" className="p-5">
+            <h2>About Me</h2>
+            <p>I'm an engineer and designer with experience in both technical and human-centered problem solving. Professionally, I've demonstrated myself as a skilled and versatile Product Owner and UX Lead due to this experience. I'm seeking a challenging position to utilize my skills in driving product innovation and enhancing customer experiences.</p>
+        </section>
+    );
+};
+
+export default About;
