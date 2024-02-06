f5ffaddb5d735f146d225d76e290a9e52187e60b
michaelpmurphy14 <135272573+michaelpmurphy14@users.noreply.github.com>
2024-02-06 17:14:44 -0600

    Create Resume.js

---
+++ src/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section id="resume" className="p-5">
+            <h2>Resume</h2>
+            <p>Resume will go here</p>
+            {/* You can add a link to download the resume or display it inline */}
+        </section>
+    );
+};
+
+export default Resume;
